fde14709c04ad4f15c1190b1aa8544dea4318da0
Jahyun Oh <soharu1024@gmail.com>
2019-06-05 00:37:41 +0900

    Resolve TODO

--- test/hello-world-test.js
+++ test/hello-world-test.js
@@ -1,11 +1,15 @@
 'use strict';
 
+const assert = require('assert');
+const testConsole = require('test-console');
 const helloWorld = require('../source/Day-0/hello-world.js');
 
 describe('greeting()', () => {
   it('should log "Hello, World!" and parameterValue', () => {
-    const parameterValue = 'Hi';
-    helloWorld.greeting(parameterValue);
-    // TODO: check logs
+    const parameterValue = 'Welcome to 10 Days of JavaScript!';
+    const actual = testConsole.stdout.inspectSync(() => {
+      helloWorld.greeting(parameterValue);
+    });
+    assert.deepStrictEqual(actual, ['Hello, World!\n', parameterValue + '\n']);
   });
 });
