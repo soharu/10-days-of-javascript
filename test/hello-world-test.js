3e1f9b865fe8f68509a0330dffedbb5a76787cd7
Jahyun Oh <soharu1024@gmail.com>
2019-06-06 23:16:43 +0900

    Modify test description

--- test/hello-world-test.js
+++ test/hello-world-test.js
@@ -4,12 +4,14 @@ const assert = require('assert');
 const testConsole = require('test-console');
 const helloWorld = require('../source/Day-0/hello-world.js');
 
-describe('greeting()', () => {
-  it('should log "Hello, World!" and parameterValue', () => {
-    const parameterValue = 'Welcome to 10 Days of JavaScript!';
-    const actual = testConsole.stdout.inspectSync(() => {
-      helloWorld.greeting(parameterValue);
+describe('Day 0: Hello, World!', () => {
+  describe('greeting()', () => {
+    it('should log "Hello, World!" and parameterValue', () => {
+      const parameterValue = 'Welcome to 10 Days of JavaScript!';
+      const actual = testConsole.stdout.inspectSync(() => {
+        helloWorld.greeting(parameterValue);
+      });
+      assert.deepStrictEqual(actual, ['Hello, World!\n', parameterValue + '\n']);
     });
-    assert.deepStrictEqual(actual, ['Hello, World!\n', parameterValue + '\n']);
   });
 });
